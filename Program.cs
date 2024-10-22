7398a0ee36be915fae7953f78feb71288201e8be
Josie Cialdini <jcialdini@my.wctc.edu>
2024-10-22 12:59:14 -0500

    [1.08] last character

--- Program.cs
+++ Program.cs
@@ -31,8 +31,11 @@ Console.Clear();
 //   Console.WriteLine(character.Display());
 // }
 
-// display characters 6-10
-foreach(Character character in characters.Skip(5).Take(5))
-{
-  Console.WriteLine(character.Display());
-}
+// // display characters 6-10
+// foreach(Character character in characters.Skip(5).Take(5))
+// {
+//   Console.WriteLine(character.Display());
+// }
+
+// display last character
+Console.WriteLine(characters.Last().Display());
